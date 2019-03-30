f3a84c6a0ebfb52de7ca723b343c88aa6f6d25af
bensonodede <bensonodede@gmail.com>
2019-03-31 01:52:21 +0300

    removed unused variables

--- src/components/seller/AddImage.js
+++ src/components/seller/AddImage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
 import "../../styles/index.css";
