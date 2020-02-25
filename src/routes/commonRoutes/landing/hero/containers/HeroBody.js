e7900c5c261b179ffee4e9baf9f36b67ea0a129c
bensonodede <bensonodede@gmail.com>
2020-02-25 11:07:22 +0300

    center align hero body

--- src/routes/commonRoutes/landing/hero/containers/HeroBody.js
+++ src/routes/commonRoutes/landing/hero/containers/HeroBody.js
@@ -9,23 +9,23 @@ const HeroBody = () => (
     <div className="container">
       <div className="columns is-mobile is-multiline is-centered">
         <div className="column is-10">
-          {/* Hero title */}
-          <h1 className="title is-size-2-mobile is-size-3-tablet is-size-2-desktop is-marginless has-text-centered has-text-black">
-            Simple, beautiful online stores.
-            <br />
-          </h1>
+          <div className="hero-body--column">
+            {/* Hero title */}
+            <h1 className="title is-size-2-mobile is-size-3-tablet is-size-1-desktop is-marginless has-text-centered">
+              Simple, beautiful online stores.
+              <br />
+            </h1>
 
-          {/* Hero sub-title */}
-          <p className="has-text-centered is-size-6 has-text-grey-darker">
-            Give your customers a delightful shopping experience.
-          </p>
-        </div>
+            {/* Hero sub-title */}
+            <p className="has-text-centered is-size-6">
+              Give your customers a delightful shopping experience.
+            </p>
 
-        {/* Sign up button */}
-        <div className="column is-10 has-text-centered is-centered is-center">
-          <Link to="/login">
-            <Button>Get started</Button>
-          </Link>
+            {/* Sign up button */}
+            <Link to="/login">
+              <Button>Get started</Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
